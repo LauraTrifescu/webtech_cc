8086fcb89076ffa130468196cca807c1459f10c9
Laura <lauratrifescu@gmail.com>
2020-05-06 16:05:15 +0000

    Add run script

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use('/',express.static('frontend'))
 
 
 app.get('/hello',(request,response) => {
-    response.status(200).json({hello:"world"})
+    response.status(200).json({hello: process.env})
 })
 
 app.get('/test',(req,res)=> {
@@ -107,4 +107,4 @@ app.delete('/messages/:id', (request, response) => {
     })
 })
 
-app.listen(8080)
+app.listen(process.env.PORT||8080)
